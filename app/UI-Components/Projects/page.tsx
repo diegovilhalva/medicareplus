de2228581ab40bfd72e07c95e6a30333b1e190b8
Diego Vilhalva <diegorocha450@yahoo.com>
2025-10-23 11:49:46 -0300

    create projects page

---
+++ app/UI-Components/Projects/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProjectDetails from './ProjectDetails/page'
+import Project from './Project/page'
+
+const page = () => {
+    return (
+        <>
+            <Project />
+        </>
+    )
+}
+
+export default page
